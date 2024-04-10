20724764e80686e1526d4a3e43625c288e1b56e6
thaua-arruda <thauafarruda@gmail.com>
2024-04-10 15:51:19 -0400

    add questao 6

---
+++ questao6.js
@@ -0,0 +1,9 @@
+/*
+Questão 6: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um número é múltiplo de 5. A variável numero contém o valor a 
+ser verificado. Atribua o resultado à variável resultado.
+*/
+
+let numero = 5;
+let resultado = (numero % 5 === 0) ? "É um múltiplo de 5" : "Não é um múltiplo";
+console.log(resultado);
